4008333e07005e8da3447bd1d9940c2b8b9532d1
KaioQuaresmadSouza <kaioquaresmadesouza1@gmail.com>
2025-08-28 23:45:58 -0300

    Espírito Santo(feat: Feito a criação do service que vai fazer a criação do register

--- FinMeha/src/app/features/auth/components/register/register.component.ts
+++ FinMeha/src/app/features/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +14,7 @@ export class RegisterComponent {
 
   registerForm : FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, private authService: AuthService){
     this.registerForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -22,7 +24,22 @@ export class RegisterComponent {
   }
   onSubmit() :void{
     if(this.registerForm.valid){
-      console.log(this.registerForm.value)
-    }
+      this.authService.register(this.registerForm.value).subscribe((response)=> {
+        // Este bloco recebe o "sinal" da API
+        console.log("Registro bem-sucedido!", response);
+
+        if(response && response.token){
+          //Armazena o token na variável local do navegador
+          localStorage.setItem('auth_token', response.token);
+          console.log('Token armazenado:', response.token);
+          //Você pode adicionar uma navegação para outra página aqui
+        }
+      },
+      (error) => {
+        //Esse bloco lida com erros da API
+        console.error('Erro no registro!', error);
+        // Mostre uma mensagem de erro ao usuário
+      }
+    )}
   }
 }
